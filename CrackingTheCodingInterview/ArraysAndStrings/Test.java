04bb2be8b9990aa52587400f9ca9dfece4c39d36
Pulkit <pulkit3012@gmail.com>
2026-05-15 09:57:29 +0100

    Added a main class to display the output of all the different questions in Arrays and Strings for test data

---
+++ CrackingTheCodingInterview/ArraysAndStrings/Test.java
@@ -0,0 +1,69 @@
+package ArraysAndStrings;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Testing Arrays and Strings");
+        testQ1();
+        testQ2();
+        testQ3();
+        testQ4();
+        testQ5();
+        testQ6();
+    }
+
+
+    public static void testQ1() {
+        System.out.println("Testing Q1_IsUnique");
+        System.out.println("hello = " + Q1_IsUnique.isUnique("hello"));
+        System.out.println("world = " + Q1_IsUnique.isUnique("world"));
+        System.out.println("Testing Q1_IsUnique approach 2");
+        System.out.println("hello = " + Q1_IsUnique.isUnique2("hello"));
+        System.out.println("world = " + Q1_IsUnique.isUnique2("world"));
+        System.out.println();
+    }
+
+
+    public static void testQ2() {
+        System.out.println("Testing Q2_CheckPermutation");
+        System.out.println("hello, oellh = " + Q2_CheckPermutation.isPermutation("hello", "oellh"));
+        System.out.println("hello, world = " + Q2_CheckPermutation.isPermutation("hello", "world"));
+        System.out.println("hello, oellh = " + Q2_CheckPermutation.isPermutation2("hello", "oellh"));
+        System.out.println("hello, world = " + Q2_CheckPermutation.isPermutation2("hello", "world"));
+        System.out.println();
+    }
+
+
+    public static void testQ3() {
+        System.out.println("Testing Q3_URLify");
+        System.out.println(Q3_URLify.URLCreator("Mr John Smith    ", 13));
+        System.out.println();
+    }
+
+
+    public static void testQ4() {
+        System.out.println("Testing Q4_PalindromePermutation");
+        System.out.println("tact coa = " + Q4_PalindromePermutation.hasPalindromePermutation("tact coa"));
+        System.out.println("hello = " + Q4_PalindromePermutation.hasPalindromePermutation("hello"));
+        System.out.println();
+    }
+
+
+    public static void testQ5() {
+        System.out.println("Testing Q5_OneAway");
+        System.out.println("pale, ple -> " + Q5_OneAway.oneAway("pale", "ple"));
+        System.out.println("pales, pale -> " + Q5_OneAway.oneAway("pales", "pale"));
+        System.out.println("pale, bale -> " + Q5_OneAway.oneAway("pale", "bale"));
+        System.out.println("pale, bake -> " + Q5_OneAway.oneAway("pale", "bake"));
+        System.out.println();
+    }
+
+
+    public static void testQ6() {
+        System.out.println("Testing Q6_StringCompression");
+        String test = "aabcccccaaa";
+        System.out.println(test + " = " + Q6_StringCompression.compressed(test));
+        System.out.println();
+    }
+    
+}
